ceea85c5d0f0568df1cf18dcb761027e5f8f5a16
INGABIRE RUTAGENGWA Diane <ingabirerdianna250@gmail.com>
2024-02-13 04:33:21 -0800

    initial commit

--- test_03/play.js
+++ test_03/play.js
@@ -1,10 +1,15 @@
-const stove= require("./stove");
+// const stove= require("./stove");
 
-function pot(handle1, handle2) {
-    // console.log(handle1.name);
-    // console.log(handle2.name);
-    console.log(handle1.name + " is on the " + handle2.name);
+// function pot(handle1, handle2) {
+//     // console.log(handle1.name);
+//     // console.log(handle2.name);
+//     console.log(handle1.name + " is on the " + handle2.name);
+// }
+
+
+// stove(pot);
+
+const name = "diane";
+if(name =="diane"){
+    console.log("hello diane");
 }
-
-
-stove(pot);
